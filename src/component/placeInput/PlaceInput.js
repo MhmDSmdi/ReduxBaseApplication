4da03d80533ae259237c40baff575875d6a47825
MhmdSmdi <mhmd.samadi76@gmail.com>
2018-07-28 00:28:02 +0430

    FlatList added

--- src/component/placeInput/PlaceInput.js
+++ src/component/placeInput/PlaceInput.js
@@ -18,9 +18,9 @@ class PlaceInput extends Component {
     }
   
     this.props.onPlaceAdded(this.state.placeName);
-    // this.setState({
-    //     placeName : ''
-    // });
+    this.setState({
+        placeName : ''
+    });
 
   };
 
